8d345732ccde3ecbecf19f5cde93bdacca1f0f04
amrs12145 <amrs12145@gmail.com>
2026-02-25 15:33:21 -0600

    Problem 2

---
+++ hwa-lesson3/src/P2TimeZones/problem2.java
@@ -0,0 +1,32 @@
+package P2TimeZones;
+
+import javax.swing.text.DateFormatter;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class problem2 {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the event Name");
+        String name = sc.next();
+        System.out.println("Enter the event Date in yyyy/MM/dd format");
+        String date = sc.next();
+        LocalDate formattedDate = LocalDate.parse(date,DateTimeFormatter.ofPattern("yyyy/MM/dd") );
+        System.out.println("Enter the event Time in HH:mm format");
+        String time = sc.next();
+        LocalTime formattedTime = LocalTime.parse(time);
+
+        Event event = new Event(name,formattedDate,formattedTime);
+
+        event.displayDetails();
+
+
+
+
+
+    }
+
+
+}
