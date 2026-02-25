d723d6b28357398757a009e0c69270932f3d4ad3
Phuong Nguyen <miu.nthphuong@gmail.com>
2026-02-26 03:19:46 +0700

    Create code structure
    Create code structure

---
+++ hwa-lesson3/src/P1ClassObjects/Customer.java
@@ -0,0 +1,13 @@
+package P1ClassObjects;
+
+public class Customer {
+    //firstName, lastName, socSecurityNum
+    public String firstName;
+    public String lastName;
+    public String socSecurityNum;
+    public Customer(String firstName, String lastName, String socSecurityNum) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.socSecurityNum = socSecurityNum;
+    }
+}
