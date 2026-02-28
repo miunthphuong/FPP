94e28eedb92f4e393fc0d20b71a18fc51612934f
Phuong Nguyen <miu.nthphuong@gmail.com>
2026-02-28 10:28:18 +0700

    P1 Find min character in String
    Using recursion to find the minimum character in the given string input

--- hwa-lesson4/src/P1MinimumCharacter/P1Main.java
+++ hwa-lesson4/src/P1MinimumCharacter/P1Main.java
@@ -1,4 +1,23 @@
 package P1MinimumCharacter;
 
 public class P1Main {
+    public static void main(String[] args) {
+        String input = "MinString";
+        char result = findMinCharacter(input, 0);
+        System.out.println("The minimum character in the string \"" + input + "\" is: '" + result + "'");
+    }
+
+    public static char findMinCharacter(String str, int index) {
+        if (index == str.length() - 1) {
+            return str.charAt(index);
+        }
+        
+        char minRemainder = findMinCharacter(str, index + 1);
+
+        if (str.charAt(index) < minRemainder) {
+            return str.charAt(index);
+        } else {
+            return minRemainder;
+        }
+    }
 }
