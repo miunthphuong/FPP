4f11d06b67ac6aa88f341c155dfe52532552b9f7
Phuong Nguyen <miu.nthphuong@gmail.com>
2026-03-01 03:19:24 +0700

    - Update P2 testcases
    - Update P2 testcases

--- hwa-lesson4/src/P2Reversing/P2Main.java
+++ hwa-lesson4/src/P2Reversing/P2Main.java
@@ -3,18 +3,18 @@ import java.util.Arrays;
 
 public class P2Main {
     public static void main(String[] args) {
-        int[] arr = {1, 3, 5, 7, 9, 13};
-
-        System.out.println("Before Reverse: " + Arrays.toString(arr));
-        reverseData(arr);
-        System.out.println("After Reverse: " + Arrays.toString(arr));
+        int[] arrBefore = {1, 3, 5, 7, 9, 13};
+        System.out.println("Before Reverse: " + Arrays.toString(arrBefore));
+        int[] arrAfter = reverseData(arrBefore);
+        System.out.println("After Reverse: " + Arrays.toString(arrAfter));
     }
 
-    public static void reverseData(int[] array) {
+    public static int[] reverseData(int[] array) {
         if (array == null || array.length <= 1) {
-            return;
+            return array;
         }
         reverseRecursive(array, 0, array.length - 1);
+        return array;
     }
 
     private static void reverseRecursive(int[] arr, int left, int right) {
@@ -24,7 +24,6 @@ public class P2Main {
         int temp = arr[left];
         arr[left] = arr[right];
         arr[right] = temp;
-
         reverseRecursive(arr, left + 1, right - 1);
     }
 }
---
+++ hwa-lesson4/src/P2Reversing/P2MainTest.java
@@ -0,0 +1,50 @@
+package P2Reversing;
+
+import P1MinimumCharacter.P1Main;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class P2MainTest {
+    @Test
+    void reverseData_test1() {
+        int[] arrBefore = {1, 3, 5, 7, 9, 13};
+        String result = Arrays.toString(P2Main.reverseData(arrBefore));
+        String expectResult = "[13, 9, 7, 5, 3, 1]";
+        assertEquals(expectResult, result);
+    }
+
+    @Test
+    void reverseData_test2() {
+        int[] arrBefore = {1, 3, 7, 2, 8};
+        String result = Arrays.toString(P2Main.reverseData(arrBefore));
+        String expectResult = "[8, 2, 7, 3, 1]";
+        assertEquals(expectResult, result);
+    }
+
+    @Test
+    void reverseData_test3() {
+        int[] arrBefore = {};
+        String result = Arrays.toString(P2Main.reverseData(arrBefore));
+        String expectResult = "[]";
+        assertEquals(expectResult, result);
+    }
+
+    @Test
+    void reverseData_test4() {
+        int[] arrBefore = {-1, 3, -7, 2, 8};
+        String result = Arrays.toString(P2Main.reverseData(arrBefore));
+        String expectResult = "[8, 2, -7, 3, -1]";
+        assertEquals(expectResult, result);
+    }
+
+    @Test
+    void reverseData_test5() {
+        int[] arrBefore = {-10};
+        String result = Arrays.toString(P2Main.reverseData(arrBefore));
+        String expectResult = "[-10]";
+        assertEquals(expectResult, result);
+    }
+}
