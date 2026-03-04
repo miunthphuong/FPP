a51e2ab35628dc4546b11a014e779571ad94adc3
Phuong Nguyen <miu.nthphuong@gmail.com>
2026-03-05 06:08:43 +0700

    Solve E
    Solve E

---
+++ hwa-lesson6/src/TaskE/TaskEMain.java
@@ -0,0 +1,19 @@
+package TaskE;
+import TaskE.DeptEmployee;
+import TaskE.DeptEmployeeData;
+
+import java.util.Arrays;
+
+public class TaskEMain {
+    public static void main(String[] args) {
+        DeptEmployee[] data = DeptEmployeeData.getDeptData();
+        //Before sort;
+        System.out.println("Before sort:" + Arrays.toString(data));
+        Arrays.sort(data, (DeptEmployee p1, DeptEmployee p2) -> p1.getHireDate().compareTo(p2.getHireDate()));
+        System.out.println("After sort by Hire Date:" + Arrays.toString(data));
+        Arrays.sort(data, (DeptEmployee p1, DeptEmployee p2) -> p1.getName().compareTo(p2.getName()));
+        System.out.println("After sort by name:" + Arrays.toString(data));
+      //  Arrays.sort(data, (DeptEmployee p1, DeptEmployee p2) -> p1.getSalary().
+
+    }
+}
