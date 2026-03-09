20128b95cca749b6b9fbfde749191140aaacbb73
Phuong Nguyen <miu.nthphuong@gmail.com>
2026-03-10 02:03:43 +0700

    Initial source code
    Initial source code

---
+++ hwa-lesson8/src/prob3/OrderTest.java
@@ -0,0 +1,14 @@
+package lesson8.prob4;
+
+import java.util.*;
+
+public class OrderTest {
+    public static void main(String[] args) {
+        // a) through i) here, print after each step (use for-each)
+    }
+
+    public static List<Order> listMoreThan50(List<Order> list) {
+        // return new ArrayList<>(...) with totalAmount > 50.0
+        return null;
+    }
+}
