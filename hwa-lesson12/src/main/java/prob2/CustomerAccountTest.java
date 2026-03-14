75fa1fe05f99b53f3c7a25f7970a05add6811c13
Phuong Nguyen <miu.nthphuong@gmail.com>
2026-03-15 05:31:55 +0700

    Update Prob 2
    Update Prob 2

--- hwa-lesson12/src/main/java/prob2/CustomerAccountTest.java
+++ hwa-lesson12/src/main/java/prob2/CustomerAccountTest.java
@@ -2,33 +2,32 @@ package prob2;
 
 public class CustomerAccountTest {
     public static void main(String[] args) {
-        CustomerAccount acc = new CustomerAccount("Customer name1", "Cus No 1", 500.0);
-
+        CustomerAccount acc = new CustomerAccount("Customer name1", "Cus No 1", 500.55);
         // 1. Deposit negative
         try {
-            acc.deposit(-10);
+            acc.deposit(-10.23);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
 
         // 2. Withdraw more than balance
         try {
-            acc.withdraw(1000);
+            acc.withdraw(1000.02);
         } catch (AccountException e) {
             System.out.println(e.getMessage());
         }
 
         // 3. Withdraw causing balance < 100
         try {
-            acc.withdraw(450);
+            acc.withdraw(450.13);
         } catch (AccountException e) {
             System.out.println(e.getMessage());
         }
 
         // 4. Successful operations
         try {
-            acc.deposit(2000);
-            acc.withdraw(300);
+            acc.deposit(200.20);
+            acc.withdraw(300.00);
         } catch (AccountException e) {
             System.out.println(e.getMessage());
         }
